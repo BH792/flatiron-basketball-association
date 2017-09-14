6fbf053a754facb9ef173717341a2cf09e23c164
Bobby Huang <zhenghuahuang@gmail.com>
2017-09-14 00:10:37 -0400

    added styling and error to forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
+      @error = true
       render :login
     end
   end
