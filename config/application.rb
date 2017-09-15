6e203c84c736b7027f3930440ade77e83818b772
Bobby Huang <zhenghuahuang@gmail.com>
2017-09-15 14:37:44 -0400

    changed img paths

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module FlatironBasketballAssociation
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
+    config.serve_static_assets = true
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
