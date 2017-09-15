c776da15c8569b996c6d7e4a5004f45010725a94
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-09-15 13:12:10 -0400

    add error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
-
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       @error = true
+      flash.now[:danger] = 'Invalid email/password combination'
       render :login
     end
   end
